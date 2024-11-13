1f9eab301992872558e9b549f1f41bd1c94d8367
s-davari <s-davari@users.noreply.github.com>
2024-11-13 18:54:12 +0000

    deploy: fc3a69d64ae6a3d37bf15d9a645d4295000cf02d

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ base_info = {
     '3DILAB': 'https://wordpress.cs.vt.edu/3digroup/author/sdavari/',
     'RESUME':'https://s-davari.github.io/Resume-ShakibaDavari.pdf',
     'CV':'https://s-davari.github.io/CV-ShakibaDavari.pdf',
+    'DISSERTATION':'https://vtechworks.lib.vt.edu/items/9c1f5ba4-fcb0-4825-b8d3-f4d32c98c576',
     'WEBSITE': 's-davari.github.io',
     "show_about":True,
     "show_edu":True,
@@ -292,6 +293,9 @@ def resume_grab():
 def email_grab():
     return page_redirect(base_info['EMAIL'])
 
+@app.route('/dissertation')
+def dissertation_grab():
+    return page_redirect(base_info['DISSERTATION'])
 
 @app.route('/cv')
 def cv_grab():
